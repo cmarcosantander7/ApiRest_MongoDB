5141ec6f30b37f76ff8ff419e16fdc1be0b4a70e
Marco S <marcosantander989@gmail.com>
2022-03-16 20:59:38 -0500

    API mongo local 100%

---
+++ crud/src/main/java/com/ms/crud/controllers/ProductoController.java
@@ -0,0 +1,39 @@
+package com.ms.crud.controllers;
+
+
+import com.ms.crud.models.ProductoDTO;
+import com.ms.crud.repository.IProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT,
+        RequestMethod.DELETE })
+@RequestMapping("/api/products")
+public class ProductoController {
+    @Autowired
+    private IProductDAO repository;
+
+    @PostMapping("/product")
+    public ProductoDTO create(@Validated @RequestBody ProductoDTO p) {
+        return repository.insert(p);
+    }
+
+    @GetMapping("/")
+    public List<ProductoDTO> readAll() {
+        return repository.findAll();
+    }
+
+    @PutMapping("/product/{id}")
+    public ProductoDTO update(@PathVariable String id, @Validated @RequestBody ProductoDTO p) {
+        return repository.save(p);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public void delete(@PathVariable String id) {
+        repository.deleteById(id);
+    }
+}
---
+++ crud/src/main/java/com/ms/crud/repository/IProductDAO.java
@@ -0,0 +1,9 @@
+package com.ms.crud.repository;
+
+import com.ms.crud.models.ProductoDTO;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductDAO extends MongoRepository<ProductoDTO, String> {
+}
